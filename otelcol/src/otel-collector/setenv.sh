0e0f5fec2fa7232134ea8e24d57b8e6d376117fe
wilson-dynatrace <53265413+wilson-dynatrace@users.noreply.github.com>
2025-11-20 11:47:39 +0800

    Delete otelcol/src/otel-collector/setenv.sh

--- otelcol/src/otel-collector/setenv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export DT_ENDPOINT=https://{your-env-id}.live.dynatrace.com/api/v2/otlp 
-export DT_API_TOKEN=dt0c01.MY_SECRET_TOKEN 
-export OTEL_EXPORTER_OTLP_METRICS_TEMPORALITY_PREFERENCE=delta#!/bin/bash
-echo $DT_ENDPOINT
-echo $DT_API_TOKEN
-echo $OTEL_EXPORTER_OTLP_METRICS_TEMPORALITY_PREFERENCE
