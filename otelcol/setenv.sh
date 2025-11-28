8dd3744816a85590035bf970e196e8d8adbae618
wilson-dynatrace <53265413+wilson-dynatrace@users.noreply.github.com>
2025-11-28 10:36:53 +0800

    Update setenv.sh

--- otelcol/setenv.sh
+++ otelcol/setenv.sh
@@ -3,7 +3,7 @@
 # export DT_ENDPOINT=https://{your-env-id}.live.dynatrace.com/api/v2/otlp
 export DT_ENDPOINT=https://eou37502.apps.dynatrace.com/api/v2/otlp
 export DT_API_TOKEN=dt0c01.MY_SECRET_TOKEN 
-export OTEL_EXPORTER_OTLP_METRICS_TEMPORALITY_PREFERENCE=delta#!/bin/bash
+export OTEL_EXPORTER_OTLP_METRICS_TEMPORALITY_PREFERENCE=delta
 echo $DT_ENDPOINT
 echo $DT_API_TOKEN
 echo $OTEL_EXPORTER_OTLP_METRICS_TEMPORALITY_PREFERENCE
