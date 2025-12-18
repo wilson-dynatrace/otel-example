aa58db1d9d5fbcdd6f9b6b9fe2116f76ad59c808
wilson-dynatrace <53265413+wilson-dynatrace@users.noreply.github.com>
2025-12-18 10:39:12 +0800

    Update setup.sh

--- php/setup.sh
+++ php/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Install latest PHP 8.4 from Ondřej Surý PPA
+sudo apt install software-properties-common python3-launchpadlib python3-apt -y
 sudo add-apt-repository ppa:ondrej/php -y
 sudo apt update
 sudo apt install php8.4 php8.4-cli php8.4-dev php8.4-common ...
